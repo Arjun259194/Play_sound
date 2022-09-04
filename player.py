9480d7af6ba4d13cf3173852ab9f20cc6e8b1f53
arjun <arjun259194@gmail.com>
2022-09-04 16:07:13 +0530

    added user input

--- player.py
+++ player.py
@@ -1,6 +1,13 @@
+from asyncio import sleep
 from playsound import playsound
-file = './music/pasoori.mp3'
 
-print('playing music....')
+file = input('Enter sound file location with file\n>')
+opened_file = open(file)
+file_dir = opened_file.name
+opened_file.close()
+
+file_name = file_dir.split('/')[-1].split('.')[0]
+
+print(f'playing {file_name} ...')
 
 playsound(file)
